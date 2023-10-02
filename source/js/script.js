c9a32a40aed23cf2c86634a69a0d6874f88c3582
Jose <pauloowneto@gmail.com>
2023-10-02 00:12:56 -0300

    Update before of purgeCSS

--- source/js/script.js
+++ source/js/script.js
@@ -12,17 +12,17 @@ let sections = document.querySelectorAll('.section');
 let navLinks = document.querySelectorAll('header nav ul li a');
 
 window.onscroll = () => {
-    sections.forEach(sec =>{
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             // active navbar links
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
             })
             // active section for animation on scroll
             sec.classList.add('show-animate');
@@ -39,9 +39,6 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toogle icon and navbar when click navbar links (scroll)
-
     menuIcon.classList.remove('active');
     navbar.classList.remove('active');
-
-
 }
---
+++ source/purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ['index.html', 'js/script.js'], // especifica todos os arquivos HTML e JS no projeto
+    css: ['./style.css'],
+    output: './dist/',
+  }
+  
